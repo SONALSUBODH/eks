9369d44dae0f35e658eeb25713f83ccdbcc2075a
SONALSUBODH <ms.sonalkulkarni@gmail.com>
2025-08-02 20:03:21 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,54 @@
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "eks-cluster-example"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "sts:AssumeRole",
+          "sts:TagSession"
+        ]
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster_role.name
+}
+    resource "aws_iam_role" "eks_node_role" {
+      name = "eks-node-role"
+      role = aws_iam_role.eks_node_role.name
+
+      assume_role_policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+          {
+            Effect = "Allow"
+            Principal = {
+              Service = "ec2.amazonaws.com"
+            }
+            Action = "sts:AssumeRole"
+          }
+        ]
+      })
+    }
+[
+    aws_iam_role_policy_attachment.example-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.example-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.example-AmazonEC2ContainerRegistryReadOnly,
+  ]
+}
+data "aws_vpc" "default" {
+  default = true
+}
+data "aws_subnets" "default" {
+     filter {
+     name = "vpc-id"
+     value = [data.aws_vpc.default.id]
+    }
+}
