5878191c9da209d3c0db797795ab600bf1decc56
SONALSUBODH <ms.sonalkulkarni@gmail.com>
2025-08-03 11:43:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,10 +18,10 @@ resource "aws_iam_role" "eks_cluster_role" {
 }
 resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.eks_node_role.name
+  role       = aws_iam_role.eks_cluster_role.name
 }
-    resource "aws_iam_role" "example" {
-  name = "eks-node-group-example"
+    resource "aws_iam_role" "eks_node_role" {
+  name = "eks-node-role"
 
   assume_role_policy = jsonencode({
     Statement = [{
@@ -37,17 +37,17 @@ resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSClusterPolicy" {
 
 resource "aws_iam_role_policy_attachment" "example-AmazonEKSWorkerNodePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.example.name
+  role       = aws_iam_role.eks_node_role.name
 }
 
 resource "aws_iam_role_policy_attachment" "example-AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.example.name
+  role       = aws_iam_role.eks_node_role.name
 }
 
 resource "aws_iam_role_policy_attachment" "example-AmazonEC2ContainerRegistryReadOnly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.example.name
+  role       = aws_iam_role.eks_node_role.name
 }
 }
 data "aws_vpc" "default" {
